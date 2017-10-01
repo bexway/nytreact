141bb827e08dd4b5c15fe4ace6527c1b1ebe092e
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-10-01 17:05:26 -0500

    added main component frame

---
+++ client/src/pages/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Main extends Component {
+  state = {
+  };
+
+    render() {
+        return (
+            <p>Hello world!</p>
+        )
+    }
+}
+
+export default Main;
